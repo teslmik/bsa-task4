7c2792d803e10be6732c92cc6da3580d4618a3bd
Teslmik <teslmik@gmail.com>
2022-05-20 17:00:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // todo: add logic
+
+"use strict"
+
+const slide = document.querySelectorAll('.slide');
+
+slide.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    const target = e.target.closest('.slide');
+    removeClass();
+    target.classList.add('active');
+  });
+});
+
+const removeClass = () => slide.forEach((item) => item.classList.remove('active'));
